8e08de2bda94c7ffe067397a81eb50e39b12fe99
LayreBoi <78430587+LayreBoi@users.noreply.github.com>
2021-04-18 21:19:27 +0200

    fixed something

---
+++ cssjs/UJ-Wk95YFU.js
@@ -0,0 +1,4 @@
+if (/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)){
+     window.onload = function (){
+         document.getElementById('nomob').style.display = 'none';
+}}

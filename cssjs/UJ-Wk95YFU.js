755dee2b0512c077721be74937dfdac858702498
LayreBoi <78430587+LayreBoi@users.noreply.github.com>
2021-05-08 12:00:47 +0200

    Updating website...
    From just HTML, JS and CSS it's now Vue

--- cssjs/UJ-Wk95YFU.js
+++
@@ -1,4 +0,0 @@
-if (/Android|webOS|iPhone|iPad|iPod|pocket|psp|kindle|avantgo|blazer|midori|Tablet|Palm|maemo|plucker|phone|BlackBerry|symbian|IEMobile|mobile|ZuneWP7|Windows Phone|Opera Mini/i.test(navigator.userAgent)){
-     window.onload = function (){
-         document.getElementById('nomob').style.display = 'none';
-}}
